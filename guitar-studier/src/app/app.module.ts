ef3b2bda47d4ad6a1ad85e182df5ac4813e37745
Ben Lutz <blutz@billtrust.com>
2019-06-08 11:44:14 -0400

    adding in page not found and trying to get links to work

--- guitar-studier/src/app/app.module.ts
+++ guitar-studier/src/app/app.module.ts
@@ -4,22 +4,30 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { JazzComponent } from './jazz/jazz.component';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent, data: { title: 'Home' }, pathMatch: 'full'  },
+  { path: 'jazz', component: JazzComponent, data: { title: 'Jazz' }  },
+  { path: '**', component: PageNotFoundComponent, data: { title: 'Page Not Found' } }
+];
+
 
 @NgModule({
   declarations: [
     AppComponent,
     JazzComponent,
-    HomeComponent
+    HomeComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot([
-      { path: '', component: HomeComponent },
-      { path: 'jazz', component: JazzComponent },
-    ])
+    RouterModule.forRoot(
+      appRoutes,
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
