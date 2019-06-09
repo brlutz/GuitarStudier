10f990ba36cc14112782b4563f8f753c2b2b88dd
Ben Lutz <blutz@billtrust.com>
2019-06-08 20:58:10 -0400

    added content

--- guitar-studier/src/app/home/home.component.ts
+++ guitar-studier/src/app/home/home.component.ts
@@ -10,7 +10,7 @@ export class HomeComponent implements OnInit {
   title = 'Guitar Studier';
   // tslint:disable-next-line:max-line-length
   jumbotronText1 = 'For intermediate guitar, there is no better way to get better than to study guitar chords all over the neck and their relations to scales.';
-  jumbotronText2 = 'Use the guitar studies and exercises below to improve your intermediate guitar.';
+  jumbotronText2 = 'Use the guitar studies and exercises below to improve your intermediate guitar skills.';
 
   constructor() { }
 
