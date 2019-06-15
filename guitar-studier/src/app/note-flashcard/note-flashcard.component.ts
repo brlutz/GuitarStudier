5070e4c1e19c30b9951f008533e1104b99c69d88
Ben Lutz <blutz@billtrust.com>
2019-06-15 10:15:43 -0400

    working random notes

--- guitar-studier/src/app/note-flashcard/note-flashcard.component.ts
+++ guitar-studier/src/app/note-flashcard/note-flashcard.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var Vex: any;
 @Component({
   selector: 'app-note-flashcard',
   templateUrl: './note-flashcard.component.html',
@@ -14,50 +14,69 @@ export class NoteFlashcardComponent implements OnInit {
   }
 
   SightReading() {
-    var VF = Vex.Flow;
+    const VF = Vex.Flow;
 
     // Create an SVG renderer and attach it to the DIV element named "boo".
-    var div = document.getElementById("boo")
-    var renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
+    const div = document.getElementById('boo');
+    div.innerHTML = '';
+    const renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
 
     // Configure the rendering context.
     renderer.resize(500, 500);
-    var context = renderer.getContext();
-    context.setFont("Arial", 10, "").setBackgroundFillStyle("#eed");
+    const context = renderer.getContext();
+    context.setFont('Arial', 10, '').setBackgroundFillStyle('#eed');
 
     // Create a stave of width 400 at position 10, 40 on the canvas.
-    var stave = new VF.Stave(10, 40, 400);
+    const stave = new VF.Stave(10, 40, 400);
 
     // Add a clef and time signature.
-    stave.addClef("treble").addTimeSignature("4/4");
+    stave.addClef('treble').addTimeSignature('4/4');
 
-    var notes = [
+
+
+
+    const notes = [
       // A quarter-note C.
-      new VF.StaveNote({ clef: "treble", keys: ["c/4"], duration: "q" }),
+      new VF.StaveNote({ clef: 'treble', keys: [this.getKeys()], duration: 'q' }),
 
       // A quarter-note D.
-      new VF.StaveNote({ clef: "treble", keys: ["d/4"], duration: "q" }),
+      new VF.StaveNote({ clef: 'treble', keys: ['d/4'], duration: 'q' }),
 
       // A quarter-note rest. Note that the key (b/4) specifies the vertical
       // position of the rest.
-      new VF.StaveNote({ clef: "treble", keys: ["b/4"], duration: "qr" }),
+      new VF.StaveNote({ clef: 'treble', keys: ['b/4'], duration: 'qr' }),
 
       // A C-Major chord.
-      new VF.StaveNote({ clef: "treble", keys: ["c/4", "e/4", "g/4"], duration: "q" })
+      new VF.StaveNote({ clef: 'treble', keys: ['c/4', 'e/4', 'g/4'], duration: 'q' })
     ];
 
     // Create a voice in 4/4 and add above notes
-    var voice = new VF.Voice({ num_beats: 4, beat_value: 4 });
+    const voice = new VF.Voice({ num_beats: 4, beat_value: 4 });
     voice.addTickables(notes);
 
     // Format and justify the notes to 400 pixels.
-    var formatter = new VF.Formatter().joinVoices([voice]).format([voice], 400);
+    const formatter = new VF.Formatter().joinVoices([voice]).format([voice], 400);
 
     // Render voice
     voice.draw(context, stave);
 
     // Connect it to the rendering context and draw!
     stave.setContext(context).draw();
+  }
+
+  getNotes() {
+    const octave = '4';
+    const notesStrings = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
+
+    return notesStrings[Math.floor(Math.random() * notesStrings.length)];
+  }
+
+  getKeys() {
+    return this.getNotes() + '/' + this.getOctave();
+  }
+
+  getOctave() {
+    return '4';
   }
 
 }
